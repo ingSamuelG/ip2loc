fc06553856212eee251497dd113a0dfdd6e39f81
ingsissam <33227372+ingsissam@users.noreply.github.com>
2023-01-04 01:04:24 -0400

    feat: Add a list method

--- Program.fs
+++ Program.fs
@@ -1,14 +1,12 @@
-﻿open System
-open System.Net
+﻿open System.Net
 open System.Net.Http
-open System.Net.Http.Json
 open System.IO
 
 let isValidIPv4 (ip: string) =
     let isValid = IPAddress.TryParse(ip)
     isValid
 
-let fromIPtoLocationJson ips =
+let fromIPtoLocationJsonFromParams ips =
     let apiKey = "HCYFRHMCLD"
 
     for ip in ips do
@@ -32,8 +30,43 @@ let fromIPtoLocationJson ips =
         else
             printfn $"Error: in the response for {parseIp}"
 
+let fromIPtoLocationJsonFromList () =
+    let apiKey = "HCYFRHMCLD"
+    let rows = File.ReadAllLines "./ips/ips.txt"
+
+    for ip in rows do
+        let trimIp = ip.Trim()
+        let isValid, parseIp = isValidIPv4 trimIp
+
+        if isValid then
+            let client = new HttpClient()
+
+            let response =
+                client
+                    .GetAsync(
+                        sprintf
+                            $"https://api.ip2location.com/v2/?key={apiKey}&ip={parseIp}&format=json&package=WS25&&addon=continent,country,region,city,geotargeting,country_groupings,time_zone_info&lang=zh-cn"
+                    )
+                    .Result
+
+            let content = response.Content.ReadAsStringAsync().Result
+
+            System.IO.File.WriteAllText($"./files/{ip}.json", content)
+            printfn $"Success: in the response for {parseIp}"
+        else
+            printfn $"Error: in the response for {parseIp}"
 
 [<EntryPoint>]
 let main argv =
-    fromIPtoLocationJson argv
-    0
+    let option = argv.[0]
+    let param = argv |> Array.skip 1
+
+    if option = "list" then
+        fromIPtoLocationJsonFromList ()
+        0
+    elif option = "param" then
+        fromIPtoLocationJsonFromParams param
+        0
+    else
+        printfn "Not a valid option"
+        1
